9829bccffa4418f26ddbf31d81a7912d7ad27d7e
Alany Gabriely Lourenço <95310758+AlanyLourenco@users.noreply.github.com>
2022-05-31 16:43:56 -0300

    Create First.java

---
+++ First.java
@@ -0,0 +1,10 @@
+public class First {
+
+    public static void main(String[] args){
+        System.out.println("HELLO WORD!");
+    }
+}
+//Primeira letra em maiusculo clase/Interface Ex:Home
+//Primeira minuscula e restante camelcase Ex:hoMe Atributo, Variável ou Metodo
+//Totalmente minusculo: nome do pacote
+//Totalmente maiusculo: nome de uma constante
