ef245f6323693ba06e3cddba08697e6edfd40d8e
Alany Gabriely Lourenço <95310758+AlanyLourenco@users.noreply.github.com>
2022-06-01 16:11:50 -0300

    .

---
+++ idiomasistema/src/Idioma.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+
+public class Idioma {
+     
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println (loc.getDisplayLanguage());
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("A resolução do seu sistema é: " + d.width +" X " + d.height);
+    }
+}
