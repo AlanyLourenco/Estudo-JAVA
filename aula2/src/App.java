e073e328843660dbbf5b5ae2055ece0cedb903d7
Alany Gabriely Lourenço <95310758+AlanyLourenco@users.noreply.github.com>
2022-06-02 22:48:28 -0300

    Java estruturado, variáveis

---
+++ aula2/src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //tipo nome
+        //inteiras
+       byte numeroPequeno = 125;
+       System.out.println(numeroPequeno);
+       short numeroShort = -32768;
+       System.out.println(numeroShort);
+       int numeroInt = 2147483647;
+       System.out.println(numeroInt);
+       long numeroLong = 922337203685477000l;
+       System.out.println(numeroLong);
+
+       //ponto flutuante
+       float peso = 53.9f;
+       double pi = 3.14;
+
+       //caracter
+       char nome = 'a';
+    }
+}
+// import
+//java.applet apps
+//java.util utilitarios
+//java.math
+//java,net redes
+//javax.sound
+//javax.media
+//javax.swing interfaces graficas
+//javaFx.fxml
+
+//byte -127 a 127
+//short -32768 a 32767
+//int -2147483648 a 2147483647
+//long -922337203685477000l a 922337203685477000l
