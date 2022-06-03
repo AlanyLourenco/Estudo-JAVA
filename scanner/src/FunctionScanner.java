7eb43b0e454a562d050c4d1ff596cd98dc888521
Alany Gabriely Lourenço <95310758+AlanyLourenco@users.noreply.github.com>
2022-06-03 16:30:06 -0300

    Classe Scanner

---
+++ scanner/src/FunctionScanner.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FunctionScanner {
+    public static void main(String[] args) throws Exception {
+        int number;
+        String name;
+        
+        //scanner -> classe
+        Scanner reader= new Scanner(System.in);
+        //necessario para o uso de espaços pois define que os separadores são \r e \n
+        //[] siguinifica ou 
+        //+ significa que eles podem se repetir
+        reader.useDelimiter("[\r\n]+");
+        
+        System.out.println("Type a number ");
+        number = reader.nextInt();
+        System.out.println("Your number is "+ number);
+       
+        System.out.println("What's your name? ");
+        name = reader.next();
+        System.out.format("Your name is: %s ",name);
+    }
+}
